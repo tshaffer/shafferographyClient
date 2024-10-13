8a1d2c0f9b80f14ca4ea3492cde23dc68d01568d
Ted Shaffer <shaffer.family@gmail.com>
2024-10-13 11:25:03 -0700

    display main screen after connecting to google.

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,20 +1,43 @@
 import React, { useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
 
-const App: React.FC = () => {
-  // const navigate = useNavigate();
+import '../styles/TedTagger.css';
+import { loadMediaItems, loadKeywordData, loadTakeouts, importFromTakeout, loadDeletedMediaItems, loadLocalStorageFolders, importFromLocalStorage } from '../controllers';
+import { TedTaggerDispatch, setAppInitialized } from '../models';
+import { getKeywordRootNodeId, getPhotoLayout } from '../selectors';
+import { Button } from '@mui/material';
 
-  // useEffect(() => {
-  //   const queryParams = new URLSearchParams(window.location.search);
-  //   const accessToken = queryParams.get('accessToken');
+import Keywords from './Keywords';
+import SearchSpecDialog from './SearchSpecDialog';
+import ImportFromTakeoutDialog from './ImportFromTakeoutDialog';
+import LoupeViewController from './LoupeViewController';
+import { PhotoLayout } from '../types';
+import SurveyView from './SurveyView';
+import TopToolbar from './TopToolbar';
+import GridView from './GridView';
+import ImportFromLocalStorageDialog from './ImportFromLocalStorageDialog';
 
-  //   if (accessToken) {
-  //     localStorage.setItem('googleAccessToken', accessToken);
-  //   } else {
-  //     navigate('/');
-  //   }
-  // }, [navigate]);
+export interface AppProps {
+  photoLayout: PhotoLayout;
+  onLoadKeywordData: () => any;
+  onLoadMediaItems: () => any;
+  onLoadDeletedMediaItems: () => any;
+  onLoadTakeouts: () => any;
+  onLoadLocalStorages: () => any;
+  onSetAppInitialized: () => any;
+  keywordRootNodeId: string;
+  onImportFromTakeout: (id: string) => void;
+  onImportFromLocalStorage: (folder: string) => void;
+}
+
+
+const App = (props: AppProps) => {
+
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [showSearchSpecDialog, setShowSearchSpecDialog] = React.useState(false);
+  const [showImportFromTakeoutDialog, setShowImportFromTakeoutDialog] = React.useState(false);
+  const [showImportFromLocalStorageDialog, setShowImportFromLocalStorageDialog] = React.useState(false);
 
   const saveTokens = (accessToken: string, expiresIn: number, refreshToken: string | null) => {
     const expirationTime = Date.now() + expiresIn * 1000; // Expiration time in ms
@@ -26,13 +49,6 @@ const App: React.FC = () => {
   };
 
   useEffect(() => {
-    // const params = new URLSearchParams(window.location.search);
-    // const token = params.get('accessToken');
-    // console.log('token', token);
-    // if (token) {
-    //   localStorage.setItem('googleAccessToken', token);
-    // }
-    // Check for tokens in the URL query parameters
     const params = new URLSearchParams(window.location.search);
     const accessToken = params.get('accessToken');
     const expiresIn = params.get('expiresIn');
@@ -45,30 +61,142 @@ const App: React.FC = () => {
     if (accessToken && expiresIn) {
       saveTokens(accessToken, parseInt(expiresIn), refreshToken);
       setIsLoggedIn(true);
-      // Clear query parameters from the URL
       window.history.replaceState({}, document.title, '/');
     }
-
   }, []);
 
-  // return (
-  //   <button onClick={() => window.location.href = 'http://localhost:8080/auth/google'}>
-  //     Connect with Google
-  //   </button>
-  // );
+  React.useEffect(() => {
+    props.onLoadKeywordData()
+      .then(function () {
+        return props.onLoadTakeouts();
+      }).then(function () {
+        return props.onLoadLocalStorages();
+      }).then(function () {
+        return props.onLoadMediaItems();
+      }).then(function () {
+        return props.onLoadDeletedMediaItems();
+      }).then(function () {
+        return props.onSetAppInitialized();
+      });
+  }, []);
+
+  if (!isLoggedIn) {
+    return (
+      <a href="/auth/google">Login with Google</a>
+    );
+  }
+
+  const handleImportFromTakeout = (takeoutId: string) => {
+    props.onImportFromTakeout(takeoutId);
+  };
+
+  const handleImportFromLocalStorage = (takeoutId: string) => {
+    props.onImportFromLocalStorage(takeoutId);
+  };
+
+  const handleCloseSearchSpecDialog = () => {
+    setShowSearchSpecDialog(false);
+  };
+
+  const handleCloseImportFromTakeoutDialog = () => {
+    setShowImportFromTakeoutDialog(false);
+  };
+
+  const handleCloseImportFromLocalStorageDialog = () => {
+    setShowImportFromLocalStorageDialog(false);
+  };
+
+  const getLeftColumn = (): JSX.Element => {
+    return (
+      <div className='leftColumnStyle'>
+        <Keywords />
+        <Button onClick={() => setShowSearchSpecDialog(true)}>Set Search Spec</Button>
+        <SearchSpecDialog
+          open={showSearchSpecDialog}
+          onClose={handleCloseSearchSpecDialog}
+        />
+        <Button onClick={() => setShowImportFromTakeoutDialog(true)}>Import from Takeout</Button>
+        <ImportFromTakeoutDialog
+          open={showImportFromTakeoutDialog}
+          onImportFromTakeout={handleImportFromTakeout}
+          onClose={handleCloseImportFromTakeoutDialog}
+        />
+        <Button onClick={() => setShowImportFromLocalStorageDialog(true)}>Import from Local Storage</Button>
+        <ImportFromLocalStorageDialog
+          open={showImportFromLocalStorageDialog}
+          onImportFromLocalStorage={handleImportFromLocalStorage}
+          onClose={handleCloseImportFromLocalStorageDialog}
+        />
+      </div>
+    );
+  };
+
+  const getPhotoDisplay = (): JSX.Element => {
+    if (props.photoLayout === PhotoLayout.Loupe) {
+      return (
+        <React.Fragment>
+          <div id='centerColumn' className='centerColumnStyle'>
+            <LoupeViewController />
+          </div>
+        </React.Fragment>
+      );
+    } else if (props.photoLayout === PhotoLayout.Survey) {
+      return (
+        <React.Fragment>
+          <div id='centerColumn' className='centerColumnStyle'>
+            <SurveyView />
+          </div>
+        </React.Fragment>
+      );
+    } else {
+      return (
+        <React.Fragment>
+          {getLeftColumn()}
+          <div id='centerColumn' className='centerColumnStyle'>
+            <GridView />
+          </div>
+          <div className='rightColumnStyle'>Right Panel</div>
+        </React.Fragment>
+      );
+    }
+  };
+
+  const photoDisplay: JSX.Element = getPhotoDisplay();
 
   return (
     <div>
-      {isLoggedIn ? (
-        <p>Logged in successfully! Access token saved.</p>
-      ) : (
-        <a href="/auth/google">Login with Google</a>
-      )}
+      <React.Fragment>
+        <TopToolbar />
+      </React.Fragment>
+      <div className='appStyle'>
+        {photoDisplay}
+      </div>
     </div>
+
   );
 };
 
-export default App;
+function mapStateToProps(state: any) {
+  return {
+    photoLayout: getPhotoLayout(state),
+    keywordRootNodeId: getKeywordRootNodeId(state),
+  };
+}
+
+const mapDispatchToProps = (dispatch: TedTaggerDispatch) => {
+  return bindActionCreators({
+    onLoadKeywordData: loadKeywordData,
+    onLoadMediaItems: loadMediaItems,
+    onLoadDeletedMediaItems: loadDeletedMediaItems,
+    onSetAppInitialized: setAppInitialized,
+    onLoadTakeouts: loadTakeouts,
+    onLoadLocalStorages: loadLocalStorageFolders,
+    onImportFromTakeout: importFromTakeout,
+    onImportFromLocalStorage: importFromLocalStorage,
+  }, dispatch);
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
 
 /*
 const refreshAccessToken = async () => {
