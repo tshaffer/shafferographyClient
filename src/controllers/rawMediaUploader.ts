5587b10a153625f1ff9d5ba4ded99f1760c271f2
Ted Shaffer <shaffer.family@gmail.com>
2024-12-16 07:18:49 -0800

    include albumName in server post.

--- src/controllers/rawMediaUploader.ts
+++ src/controllers/rawMediaUploader.ts
@@ -1,17 +1,23 @@
 import { serverUrl, apiUrlFragment } from '../types';
 
+// export const uploadRawMedia = async (formData: FormData, albumName: string): Promise<any> => {
 export const uploadRawMedia = async (formData: FormData): Promise<any> => {
 
+  const albumName = 'testAlbum';
+  
   const uploadUrl = serverUrl + apiUrlFragment + 'uploadRawMedia';
 
   try {
+    // Append additional information
+    formData.append('albumName', albumName);
+
     const response = await fetch(uploadUrl, {
       method: 'POST',
       body: formData,
     });
+
     return Promise.resolve(response);
   } catch (err) {
     return Promise.reject(err);
   }
 };
-
